b37de997fe499f3b459c38080a8cc7f69326a202
Việt Anh Vũ <hunter@darkmoon.us>
2015-11-22 02:20:54 +0700

    fix load more question

--- Android/app/src/main/java/com/j4f/fragments/QuestionsFragment.java
+++ Android/app/src/main/java/com/j4f/fragments/QuestionsFragment.java
@@ -1,5 +1,7 @@
 package com.j4f.fragments;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
@@ -48,12 +50,14 @@ public class QuestionsFragment extends CoreFragment {
         return view;
     }
 
-    public int mMaxQuestion = -1;
-    public int mCurrentQuestionPage = 1;
-    public void addQuestion(Question q) {
+    public void addQuestion(final Question q) {
         questionList.add(q);
         questionAdapter.notifyDataSetChanged();
     }
+    public int mMaxQuestion = -1;
+    public int mCurrentQuestionPage = 1;
+    public int mLimit = -1;
+    @TargetApi(Build.VERSION_CODES.M)
     public void initQuestionList() {
         questionList = new ArrayList<Question>();
         questionAdapter = new QuestionAdapter(questionList, mActivity);
@@ -64,7 +68,10 @@ public class QuestionsFragment extends CoreFragment {
         questionRecyclerView.setOnLoadMoreListener(new UltimateRecyclerView.OnLoadMoreListener() {
             @Override
             public void loadMore(int itemsCount, final int maxLastVisiblePosition) {
-                getAllQuestion(Configs.QUESTION_PAGE_LIMIT, ++mCurrentQuestionPage);
+                mCurrentQuestionPage++;
+                if (mCurrentQuestionPage <= mLimit) {
+                    getAllQuestion(Configs.QUESTION_PAGE_LIMIT, mCurrentQuestionPage);
+                }
             }
         });
         getAllQuestion(Configs.QUESTION_PAGE_LIMIT, mCurrentQuestionPage);
@@ -81,6 +88,8 @@ public class QuestionsFragment extends CoreFragment {
                             if(status.equals("ok")) {
                                 if(mMaxQuestion == -1) {
                                     mMaxQuestion = response.getInt("count");
+                                    int d = mMaxQuestion / Configs.CATEGORY_PAGE_LIMIT;
+                                    mLimit = mMaxQuestion % Configs.CATEGORY_PAGE_LIMIT == 0 ? d : d + 1;
                                 }
                                 JSONArray ja = response.getJSONArray("data");
                                 for(int i=0; i<ja.length(); i++) {
@@ -99,9 +108,7 @@ public class QuestionsFragment extends CoreFragment {
                                     String timestamp = jo.getString("created_at");
 
                                     ArrayList<Category> categoriesList = new ArrayList<>();
-                                    for(String k : tag) {
-                                        categoriesList.add(new Category("ID", k, "Link", 5, 5));
-                                    }
+                                    for(String k : tag) categoriesList.add(new Category("ID", k, "Link", 5, 5));
                                     try {
                                         addQuestion(new Question(categoriesList, id, users_id, nameOfUser, avatar,
                                                 title, content, photo, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timestamp),
