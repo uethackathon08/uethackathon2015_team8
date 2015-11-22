75175b29ca9e8724d6f55931c824676bc1b39e64
Giap Nguyen <nvangiap5894@gmail.com>
2015-11-22 08:14:48 +0700

    fix upload image

--- Android/app/src/main/java/com/j4f/activities/PostQuestionActivity.java
+++ Android/app/src/main/java/com/j4f/activities/PostQuestionActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,24 +15,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.j4f.R;
 import com.j4f.application.MyApplication;
 import com.j4f.cores.CoreActivity;
-import com.j4f.utils.CustomRequest;
+import com.j4f.network.J4FClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
 
 public class PostQuestionActivity extends CoreActivity {
 
@@ -93,7 +93,7 @@ public class PostQuestionActivity extends CoreActivity {
         return true;
     }
 
-    private File createImageFile() throws IOException{
+    private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(
@@ -147,7 +147,7 @@ public class PostQuestionActivity extends CoreActivity {
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
 
             File imgFile = new File(mCurrentPhotoPath);
-            if(imgFile.exists()){
+            if (imgFile.exists()) {
 
                 Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
                 imageView.setImageBitmap(myBitmap);
@@ -160,33 +160,60 @@ public class PostQuestionActivity extends CoreActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.post_button:
-                String url = BASE_URL + "question/new";
-                Map<String, String> params = new HashMap<>();
+//                String url = BASE_URL + "question/new";
+//                Map<String, String> params = new HashMap<>();
+//                params.put("tags", "Thật là vui");
+//                params.put("title", "Vui Vui Vui : Đại ca thống");
+//                params.put("content", "Ha ha ha");
+//                params.put("users_id", MyApplication.USER_ID);
+//                File imgFile = new File(mCurrentPhotoPath);
+//                Bitmap bm = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+//                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                bm.compress(Bitmap.CompressFormat.JPEG, 50, baos); //bm is the bitmap object
+//                byte[] b = baos.toByteArray();
+//                String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+//                params.put("photo",encodedImage);
+//                Log.d("Hello world",encodedImage);
+//                CustomRequest jsObjRequest = new CustomRequest(Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        showToastLong(response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        showToastLong("Error");
+//                    }
+//                });
+//
+//                MyApplication.getInstance().addToRequestQueue(jsObjRequest);
+
+                RequestParams params = new RequestParams();
                 params.put("tags", "Thật là vui");
                 params.put("title", "Vui Vui Vui : Đại ca thống");
                 params.put("content", "Ha ha ha");
                 params.put("users_id", MyApplication.USER_ID);
-                File imgFile = new File(mCurrentPhotoPath);
-                Bitmap bm = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bm.compress(Bitmap.CompressFormat.JPEG, 50, baos); //bm is the bitmap object
-                byte[] b = baos.toByteArray();
-                String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
-                params.put("photo",encodedImage);
-                Log.d("Hello world",encodedImage);
-                CustomRequest jsObjRequest = new CustomRequest(Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
+                try {
+                    Log.e("mCurrentPhotoPath", mCurrentPhotoPath);
+                    params.put("photo", new File(mCurrentPhotoPath));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                String url = "question/new";
+                J4FClient.post(url, params, new JsonHttpResponseHandler() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        showToastLong(response.toString());
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
                     }
-                }, new Response.ErrorListener() {
+
                     @Override
-                    public void onErrorResponse(VolleyError error) {
-                        showToastLong("Error");
+                    public void onFailure(int statusCode, Header[] headers, String errorResponse, Throwable e) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                        Log.e("onFailure", e.toString());
+                        Log.e("errorResponse", errorResponse);
                     }
                 });
-
-                MyApplication.getInstance().addToRequestQueue(jsObjRequest);
                 break;
         }
     }
---
+++ Android/app/src/main/java/com/j4f/network/J4FClient.java
@@ -0,0 +1,27 @@
+package com.j4f.network;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by nvg58 on 11/22/15.
+ * Project Android
+ */
+public class J4FClient {
+    private static final String BASE_URL = "http://188.166.241.34/hackathon/j4f/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
